1a8e55ca232900db071f20f33ebc40f008ae082c
Nirmalkumar Palanisamy <nirmalkumar@mallow-tech.com>
2021-08-23 21:42:17 +0530

    ghjk

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,10 +15,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        // Admin User
         User::create([
            'name' => 'Admin',
            'email' => 'admin@devon.com',
            'password' => Hash::make('Password@123')
+        ]);
+
+        // Normal User
+        User::create([
+            'name' => 'User',
+            'email' => 'user@devon.com',
+            'password' => Hash::make('PasswordUser@123')
         ]);
     }
 }
